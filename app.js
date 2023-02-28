d14545e7cfe2bd32933713c0b6d7f1f7c6aea7ce
David Thompson <davidbthomp@gmail.com>
2023-02-28 13:34:08 -0600

    ChatGPT Start

---
+++ app.js
@@ -0,0 +1,8 @@
+// Get all the cells on the board
+const cells = document.querySelectorAll('.cell');
+
+// Set up an array to hold the numbers for the game
+const numbers = [];
+
+// Generate a new game
+function newGame()
